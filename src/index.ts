fe83992eac001a87196f378e5968136caa6b7991
SAURABHVERMA00 <saurabh2271.be22@chitkara.edu.in>
2024-09-16 23:08:06 +0530

    chore:update and add the admin route

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import express from "express";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+import adminRoutes from "@/Routes/Admin/AdminRoutes";
+import { errorMiddleware } from "@/middlewares/errorHandling";
+import cookieParser from "cookie-parser";
 
+// middlewares
+
+app.use(errorMiddleware);
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "development") {
@@ -11,6 +18,10 @@ if (process.env.NODE_ENV === "development") {
         res.send("Welcome to api set up by node-nitro ^_+");
     });
 }
+
+// admin routes
+
+app.use("/admin", adminRoutes);
 
 app.listen(PORT, () => {
     console.info(`-> now listening at http://localhost:${PORT}/`);
