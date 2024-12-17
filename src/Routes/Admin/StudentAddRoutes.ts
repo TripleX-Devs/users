bc46e50bc896f3faab4d841228b68d6b08a55f8e
rimaru06 <shivraodanger04@gmail.com>
2024-12-17 20:39:58 +0530

    studennt add Routes

--- src/Routes/Admin/StudentAddRoutes.ts
+++ src/Routes/Admin/StudentAddRoutes.ts
@@ -1,5 +1,6 @@
 
 import addStudentData from "@/Controllers/Admin/private/StudentAddData/addStudent";
+import addStudentsDataBulk from "@/Controllers/Admin/private/StudentAddData/addStudentBulk";
 import deleteStudent from "@/Controllers/Admin/private/StudentAddData/deleteStudent";
 import updateOneStudentData from "@/Controllers/Admin/private/StudentAddData/updateStudent";
 import { checkForAccessToken } from "@/middlewares/authToken";
@@ -17,6 +18,9 @@ router.use(checkRole("admin"))
 router.post("/addOneStudent",validateRequest(studentSignUpSchemaValidation) ,addStudentData)
 router.delete("/deleteStudent/:rollNo",deleteStudent);
 router.put("/updateStudent/:rollNo",validateRequest(studentUpdateSchemaValidation),updateOneStudentData);
+router.post("/addStudentInBulk",(req,res) => {
+    return res.json({message : "data in bulk processed"})
+});
 
 
 
